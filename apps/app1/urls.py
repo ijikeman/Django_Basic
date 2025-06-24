f71c7593533282a170f99ffd39fe0972c8187bee
ijikeman <keiji.ue@gmail.com>
2025-06-25 00:57:56 +0900

    一部をクラスに変更

--- apps/app1/urls.py
+++ apps/app1/urls.py
@@ -5,6 +5,6 @@ app_name = 'app1'
 
 urlpatterns = [
     path('create/', views.create_record, name='create_record'),
-    path('list/', views.list_records, name='list_records'),
+    path('list/', views.ListRecordsView.as_view(), name='list_records'),
     path('delete/<int:pk>/', views.delete_record, name='delete_record'),
 ]
--- apps/app1/views.py
+++ apps/app1/views.py
@@ -1,6 +1,8 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
 from .forms import MyForm
 from .models import MyModel
+from django.urls import reverse_lazy
 
 def create_record(request):
     if request.method == "POST":
@@ -12,9 +14,17 @@ def create_record(request):
         form = MyForm()
     return render(request, "app1/create.html", {"form": form})
 
-def list_records(request):
-    records = MyModel.objects.all()
-    return render(request, "app1/list.html", {"records": records})
+# def list_records(request):
+#     records = MyModel.objects.all()
+#     return render(request, "app1/list.html", {"records": records})
+
+# クラスに変更
+class ListRecordsView(TemplateView):
+    template_name = "app1/list.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['records'] = MyModel.objects.all()
+        return context
 
 def delete_record(request, pk):
     record = MyModel.objects.get(pk=pk)
