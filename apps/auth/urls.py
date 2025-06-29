4c415c20439daf5847bd5b195f55352c83ed3a6f
ijikeman <keiji.ue@gmail.com>
2025-06-29 23:39:23 +0900

    wip

--- apps/auth/urls.py
+++ apps/auth/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='auth/password_reset.html'), name='password_reset'),
 ]
--- apps/auth/views.py
+++ apps/auth/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
-
 class RegisterView(CreateView):
     form_class = UserCreationForm
     template_name = 'auth/register.html'
@@ -13,3 +12,12 @@ class RegisterView(CreateView):
         response = super().form_valid(form)
         login(self.request, self.object)
         return response
+
+class PasswordResetView(CreateView):
+    template_name = 'auth/password_reset.html'
+    success_url = reverse_lazy('auth:login')
+
+    # password reset function
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
